0afad9f53953bbde7ac340cd29085a8aa094f1ed
hotate1000 <ooo.336.sss@gmail.com>
2020-06-29 23:16:59 +0900

    7章完了

--- 7-02_api/extra/script.js
+++ 7-02_api/extra/script.js
@@ -1,29 +1,30 @@
 'use strict';
 
 // geolocation
+
 navigator.geolocation.getCurrentPosition(success, fail);
 
-function success(pos) {
+function success(pos){
     ajaxRequest(pos.coords.latitude, pos.coords.longitude);
 }
 
-function fail(error) {
-    alert('位置情報の取得に失敗しました。エラーコード：' + error.code);
+function fail(error){
+    alert('位置情報の取得に失敗しました。エラーコード:' + error.code);
 }
 
-// UTCをミリ秒に
-function utcToJSTime(utcTime) {
+navigator.geolocation.getCurrentPosition(success,error);
+
+function utcToJSTime(utcTime){
     return utcTime * 1000;
 }
 
-// データ取得
-function ajaxRequest(lat, long) {
+function ajaxRequest(lat,long){
     const url = 'https://api.openweathermap.org/data/2.5/forecast';
     const appId = '保存しておいたAPI Key';
 
     $.ajax({
         url: url,
-        data: {
+        data:{
             appid: appId,
             lat: lat,
             lon: long,
@@ -31,47 +32,60 @@ function ajaxRequest(lat, long) {
             lang: 'ja'
         }
     })
+    
     .done(function(data){
-        // 都市名、国名
-        $('#place').text(data.city.name + ', ' + data.city.country);
+        // console.log(data);
+        // console.log('都市名:' + data.city.name);
+        // console.log('国名:' + data.city.country);
+        $('#place').text(data.city.name + ',' + data.city.country);
 
-        // 天気予報データ
-        data.list.forEach(function(forecast, index) {
+        data.list.forEach(function(forecast, index){
             const dateTime = new Date(utcToJSTime(forecast.dt));
             const month = dateTime.getMonth() + 1;
-            const date = dateTime.getDate();
+            const date  = dateTime.getDate();
             const hours = dateTime.getHours();
-            const min = String(dateTime.getMinutes()).padStart(2, '0');
-            const temperature = Math.round(forecast.main.temp);
-            const description = forecast.weather[0].description;
-            const iconPath = `images/${forecast.weather[0].icon}.svg`;
+            const min   = String(dateTime.getMinutes()).padStart(2,'0');
 
-            // 現在の天気とそれ以外で出力を変える
-            if(index === 0) {
+
+            const description = forecast.weather[0].description;
+            const temperature = Math.round(forecast.main.temp);
+
+            const iconPath    = `images/${forecast.weather[0].icon}.svg`;
+
+            // console.log('日時' + `${month}/${date} ${hours}:${min}`);
+            // console.log('気温' + temperature);
+            // console.log('天気' + description);
+            // console.log('画像パス' + iconPath);
+
+            if(index === 0){
                 const currentWeather = `
-                <div class="icon"><img src="${iconPath}"></div>
-                <div class="info">
+                <div class='icon'><img scr="${iconPath}"></div>
+                <div class='info>
                     <p>
-                        <span class="description">現在の天気：${description}</span>
-                        <span class="temp">${temperature}</span>°C
+                        <span class='description'>現在の天気:${description}</span>
+                        <span class='temp'>${temperature}</span>°C
                     </p>
                 </div>`;
                 $('#weather').html(currentWeather);
-            } else {
+            }else{
                 const tableRow = `
                 <tr>
-                    <td class="info">
+                    <td class='into'>
                         ${month}/${date} ${hours}:${min}
                     </td>
-                    <td class="icon"><img src="${iconPath}"></td>
-                    <td><span class="description">${description}</span></td>
-                    <td><span class="temp">${temperature}°C</span></td>
+                    <td class='icon'><img src="${iconPath}"></td>
+                    <td><span class='description'>${description}</span></td>
+                    <td><span class='temp'>${temperature}°C</span></td>
                 </tr>`;
                 $('#forecast').append(tableRow);
+                
             }
         });
+
     })
-    .fail(function() {
+    .fail(function(){
         console.log('$.ajax failed!');
     })
 }
+
+
